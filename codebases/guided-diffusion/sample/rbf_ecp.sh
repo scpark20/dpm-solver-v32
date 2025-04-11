fdac1a5e5a55d70abe670e1a6765ee41970912e4
logpx <scpark20@gmail.com>
2025-04-11 12:25:55 +0900

    rbf

--- codebases/guided-diffusion/sample/rbf_ecp.sh
+++ codebases/guided-diffusion/sample/rbf_ecp.sh
@@ -1,12 +1,13 @@
 DEVICES="0"
 CONFIG="imagenet256_guided.yml"
 scale="2.0"
+order='2'
 SCALE_DIR="/data/guided-diffusion/scale/rbf_ecp"
 
 for sampleMethod in 'rbf_ecp'; do
 for steps in 5 10 15 20 25 30 35 40; do
 
-CUDA_VISIBLE_DEVICES="${DEVICES}" python sample.py --config=$CONFIG --exp=$sampleMethod"_"$steps"_scale"$scale --scale_dir=$SCALE_DIR --timesteps=$steps --sample_type=$sampleMethod --scale=$scale --lower_order_final
+CUDA_VISIBLE_DEVICES="${DEVICES}" python sample.py --order=$order --config=$CONFIG --exp=$sampleMethod"_"$steps"_scale"$scale --scale_dir=$SCALE_DIR --timesteps=$steps --sample_type=$sampleMethod --scale=$scale --lower_order_final
 
 done
 done
