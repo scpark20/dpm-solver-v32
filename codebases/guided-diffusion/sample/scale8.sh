9d06815718b3ffc6188ff0071b44a8126d2d61cf
logpx <scpark20@gmail.com>
2025-04-12 22:49:50 +0900

    scale 1 4 6 8

---
+++ codebases/guided-diffusion/sample/scale8.sh
@@ -0,0 +1,46 @@
+DEVICES="3"
+CONFIG="imagenet256_guided.yml"
+scale="8.0"
+SCALE_DIR="/data/guided-diffusion/scale/rbf_ecp_marginal"
+
+# 1) unipc order=2
+for steps in 10 20
+do
+    CUDA_VISIBLE_DEVICES="${DEVICES}" python sample.py \
+        --order=2 \
+        --config="${CONFIG}" \
+        --exp="unipc_${steps}_scale${scale}_order2" \
+        --scale_dir="${SCALE_DIR}" \
+        --timesteps="${steps}" \
+        --sample_type="unipc" \
+        --scale="${scale}" \
+        --lower_order_final
+done
+
+# 2) rbf_ecp_marginal order=2
+for steps in 10 20
+do
+    CUDA_VISIBLE_DEVICES="${DEVICES}" python sample.py \
+        --order=2 \
+        --config="${CONFIG}" \
+        --exp="rbf_ecp_marginal_${steps}_scale${scale}_order2" \
+        --scale_dir="${SCALE_DIR}" \
+        --timesteps="${steps}" \
+        --sample_type="rbf_ecp_marginal" \
+        --scale="${scale}" \
+        --lower_order_final
+done
+
+# 3) rbf_ecp_marginal order=3
+for steps in 10 20
+do
+    CUDA_VISIBLE_DEVICES="${DEVICES}" python sample.py \
+        --order=3 \
+        --config="${CONFIG}" \
+        --exp="rbf_ecp_marginal_${steps}_scale${scale}_order3" \
+        --scale_dir="${SCALE_DIR}" \
+        --timesteps="${steps}" \
+        --sample_type="rbf_ecp_marginal" \
+        --scale="${scale}" \
+        --lower_order_final
+done
