70d2f017d1e55b48e416853b10e5647cc9859f06
logpx <scpark20@gmail.com>
2025-04-13 11:08:56 +0900

    unipc200.sh

--- codebases/guided-diffusion/sample/unipc200.sh
+++ codebases/guided-diffusion/sample/unipc200.sh
@@ -1,11 +1,15 @@
-CONFIG="imagenet256_guided.yml"
-scale="0.0"
+CONFIG="imagenet256_guided_raw256.yml"
 STATS_DIR="statistics/imagenet256_guided/500_1024"
 
 for steps in 200; do
-for sampleMethod in 'unipc'; do
-
-python sample_raw.py --config=$CONFIG --exp=$sampleMethod"_"$steps"_scale"$scale --timesteps=$steps --sample_type=$sampleMethod --scale=$scale --lower_order_final
-
-done
+    for sampleMethod in 'unipc'; do
+        for scale in 1.0 2.0 4.0 6.0 8.0; do
+            python sample_raw.py --config=$CONFIG \
+                                 --exp=${sampleMethod}_${steps}_scale${scale} \
+                                 --timesteps=$steps \
+                                 --sample_type=$sampleMethod \
+                                 --scale=$scale \
+                                 --lower_order_final
+        done
+    done
 done
