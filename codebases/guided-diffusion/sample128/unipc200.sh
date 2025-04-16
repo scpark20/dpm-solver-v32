19ae194154df0fddbd5346222000d4709a7c15ca
logpx <scpark20@gmail.com>
2025-04-16 16:01:22 +0900

    imagenet128

---
+++ codebases/guided-diffusion/sample128/unipc200.sh
@@ -0,0 +1,14 @@
+CONFIG="imagenet128_guided_raw256.yml"
+
+for steps in 200; do
+    for sampleMethod in 'unipc'; do
+        for scale in 2.0 4.0 6.0 8.0; do
+            python sample_raw.py --config=$CONFIG \
+                                 --exp=${sampleMethod}_${steps}_scale${scale} \
+                                 --timesteps=$steps \
+                                 --sample_type=$sampleMethod \
+                                 --scale=$scale \
+                                 --lower_order_final
+        done
+    done
+done
