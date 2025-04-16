a529280447a175a55a1be2a880ba261a94ec0f2d
logpx <scpark20@gmail.com>
2025-04-16 13:58:38 +0900

    sample

---
+++ codebases/stable-diffusion/sample/rbf_order3_scale=7.5.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+prompts='prompts/prompts.txt'
+model='sd-v1-4'
+scale='7.5'
+
+config="configs/stable-diffusion/v1-inference.yaml"
+ckpt="models/ldm/stable-diffusion-v1/sd-v1-4.ckpt"
+H=512
+W=512
+C=4
+f=8
+STATS_DIR="statistics/sd-v1-4/${scale}_250_1024"
+SCALE_DIR="/data/ldm/scale7.5"
+sampleMethod='rbf'
+
+for order in 3
+do
+  for steps in 5 10 15 20 25 30 35 40
+  do
+    python txt2img_latent.py --fixed_code \
+      --from-file "$prompts" \
+      --steps "$steps" \
+      --statistics_dir "$STATS_DIR" \
+      --outdir "outputs/${model}/${sampleMethod}_order${order}_steps${steps}_scale${scale}" \
+      --method "$sampleMethod" \
+      --order "$order" \
+      --scale "$scale" \
+      --scale_dir "$SCALE_DIR" \
+      --config "$config" \
+      --ckpt "$ckpt" \
+      --H "$H" \
+      --W "$W" \
+      --C "$C" \
+      --f "$f"
+  
+  done
+done
