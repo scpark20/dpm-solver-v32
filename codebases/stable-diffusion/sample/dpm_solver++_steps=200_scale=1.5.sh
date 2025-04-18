e39c56bf3ec13ca8b992508d82c9f4f1093bb035
logpx <scpark20@gmail.com>
2025-04-18 13:18:27 +0900

    dpm_solver++ 200

--- codebases/stable-diffusion/sample/dpm_solver++_steps=200_scale=1.5.sh
+++ codebases/stable-diffusion/sample/dpm_solver++_steps=200_scale=1.5.sh
@@ -2,7 +2,7 @@
 
 prompts='prompts/prompts.txt'
 model='sd-v1-4'
-scale='7.5'
+scale='1.5'
 
 config="configs/stable-diffusion/v1-inference.yaml"
 ckpt="models/ldm/stable-diffusion-v1/sd-v1-4.ckpt"
@@ -14,9 +14,9 @@ STATS_DIR="statistics/sd-v1-4/${scale}_250_1024"
 
 for steps in 200
 do
-  for sampleMethod in 'uni_pc_bh2'
+  for sampleMethod in 'dpm_solver++'
   do
-    python txt2img_latent.py --fixed_code \
+    CUDA_VISIBLE_DEVICE='0' python txt2img_latent.py --fixed_code \
       --from-file "$prompts" \
       --steps "$steps" \
       --statistics_dir "$STATS_DIR" \
