9b0349650aa55b9902e0b0a4b286550444bfbd59
logpx <scpark20@gmail.com>
2025-04-18 12:13:13 +0900

    stable ddim

---
+++ codebases/stable-diffusion/sample/ddim_steps=1000_scale=5.5.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+prompts='prompts/prompts.txt'
+model='sd-v1-4'
+scale='5.5'
+
+config="configs/stable-diffusion/v1-inference.yaml"
+ckpt="models/ldm/stable-diffusion-v1/sd-v1-4.ckpt"
+H=512
+W=512
+C=4
+f=8
+
+for steps in 1000
+do
+  for sampleMethod in 'ddim'
+  do
+    python txt2img_latent.py --fixed_code \
+      --from-file "$prompts" \
+      --steps "$steps" \
+      --outdir "outputs/${model}/${sampleMethod}_steps${steps}_scale${scale}" \
+      --method "$sampleMethod" \
+      --scale "$scale" \
+      --config "$config" \
+      --ckpt "$ckpt" \
+      --H "$H" \
+      --W "$W" \
+      --C "$C" \
+      --f "$f"
+  done
+done
