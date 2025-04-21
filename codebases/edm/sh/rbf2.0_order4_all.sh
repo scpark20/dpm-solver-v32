eda69708bc0da682cd7e871d9ced6825303f8e9a
logpx <scpark20@gmail.com>
2025-04-21 14:31:09 +0900

    rbf

--- codebases/edm/sh/rbf2.0_order4_all.sh
+++ codebases/edm/sh/rbf2.0_order4_all.sh
@@ -5,6 +5,6 @@ SCALE_DIR="/data/edm/scale/rbf_ecp_marginal${SCALE}"
 
 for steps in 5 6 8 10 12 15 20 25 30 35 40; do
 
-CUDA_VISIBLE_DEVICES="4" python sample.py --sample_folder="rbf_ecp_marginal_"$steps"_"$SCALE"_"$ORDER --order=$ORDER --ckp_path=$CKPT_PATH --method=rbf_ecp_marginal --steps=$steps --skip_type=logSNR --scale_dir=$SCALE_DIR
+CUDA_VISIBLE_DEVICES="4" python sample.py --sample_folder="rbf_ecp_marginal_"$SCALE"_"$ORDER"_"$steps" --order=$ORDER --ckp_path=$CKPT_PATH --method=rbf_ecp_marginal --steps=$steps --skip_type=logSNR --scale_dir=$SCALE_DIR
 
 done
