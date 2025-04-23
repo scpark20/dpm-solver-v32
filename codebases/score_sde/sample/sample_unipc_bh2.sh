95d0df748f6b5f9ca4f81d2050377b7eb864ed81
logpx <scpark20@gmail.com>
2025-04-23 12:38:39 +0900

    v3

--- codebases/score_sde/sample/sample_unipc_bh2.sh
+++ codebases/score_sde/sample/sample_unipc_bh2.sh
@@ -1,6 +1,6 @@
 CKPT_PATH="/data/checkpoints/cifar10_ddpmpp_deep_continuous/checkpoint_8.pth"
 CONFIG="configs/vp/cifar10_ddpmpp_deep_continuous.py"
-for steps in 30 35 40 45 50; do
+for steps in 5 6 8 10 12 15 20 25 30 35 40; do
 
 if [ $steps -le 10 ]; then
     EPS="1e-3"
@@ -29,8 +29,7 @@ else
 fi
 
 #python sample.py --config=$CONFIG --ckp_path=$CKPT_PATH --sample_folder="DPM-Solver++_"$steps --config.sampling.method=dpm_solver --config.sampling.steps=$steps --config.sampling.eps=$EPS
-python sample.py --config=$CONFIG --ckp_path=$CKPT_PATH --sample_folder="UniPC_bh1_"$steps --config.sampling.method=uni_pc --config.sampling.steps=$steps --config.sampling.variant=bh1 --config.sampling.eps=$EPS
-#python sample.py --config=$CONFIG --ckp_path=$CKPT_PATH --sample_folder="UniPC_bh2_"$steps --config.sampling.method=uni_pc --config.sampling.steps=$steps --config.sampling.variant=bh2 --config.sampling.eps=$EPS
-
+#python sample.py --config=$CONFIG --ckp_path=$CKPT_PATH --sample_folder="UniPC_bh1_"$steps --config.sampling.method=uni_pc --config.sampling.steps=$steps --config.sampling.variant=bh1 --config.sampling.eps=$EPS
+python sample.py --config=$CONFIG --ckp_path=$CKPT_PATH --sample_folder="UniPC_bh2_"$steps --config.sampling.method=uni_pc --config.sampling.steps=$steps --config.sampling.variant=bh2 --config.sampling.eps=$EPS
 #python sample.py --config=$CONFIG --ckp_path=$CKPT_PATH --statistics_dir=$STATS_DIR --sample_folder="DPM-Solver-v3_"$steps --config.sampling.method=dpm_solver_v3 --config.sampling.eps=$EPS --config.sampling.steps=$steps --config.sampling.predictor_pseudo=$p_pseudo --config.sampling.use_corrector=$use_corrector --config.sampling.lower_order_final=$lower_order_final --config.sampling.corrector_pseudo=False
 done
