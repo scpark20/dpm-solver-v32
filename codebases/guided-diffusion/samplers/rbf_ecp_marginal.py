a4298cb06f31ed6e56a4d341517a2bc495c6eb46
logpx <scpark20@gmail.com>
2025-04-30 22:34:28 +0900

    lower

--- codebases/guided-diffusion/samplers/rbf_ecp_marginal.py
+++ codebases/guided-diffusion/samplers/rbf_ecp_marginal.py
@@ -413,6 +413,7 @@ class RBFSolverECPMarginal:
                denoise_to_zero=False,
                 log_scale_p=2.0,
                 log_scale_c=0.0,
+                return_intermediate=False,
                 ):
         # log_scale : predictor, corrector 모든 step에 적용할 log_scale, log_scales가 load안되면 log_scale로 작동
         # log_scales : predictor, corrector, step별로 적용할 log_scale array, shape : (2, NFE)
@@ -466,4 +467,6 @@ class RBFSolverECPMarginal:
                                               p=p, beta=beta, corrector=True, lagrange=lagrange)
                 x = x_corr
         # 최종적으로 x를 반환
+        if return_intermediate:
+            return x, [hist, lambdas, timesteps]
         return x
