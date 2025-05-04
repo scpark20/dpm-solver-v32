80b9cce23b85be364866f2ed645cc2d7894bc43c
logpx <scpark20@gmail.com>
2025-05-04 17:21:11 +0900

    dcsolver

---
+++ codebases/guided-diffusion/sample256/dcsolver_all.sh
@@ -0,0 +1,21 @@
+#DEVICES="0,1,2,3,4,5,6,7"
+DEVICES="0"
+CONFIG="imagenet256_guided.yml"
+
+for scale in 2.0 4.0 6.0 8.0; do
+    DC_DIR="/data/guided-diffusion/dc/imagenet256/scale${scale}"
+
+    for steps in 5 6 8 10 12 15 20 25; do
+        # 2) rbf_ecp_marginal order=2
+        CUDA_VISIBLE_DEVICES="${DEVICES}" python sample.py \
+            --order=3 \
+            --config="${CONFIG}" \
+            --exp="dcsolver_order3_${steps}_scale${scale}" \
+            --dc_dir="${DC_DIR}" \
+            --timesteps="${steps}" \
+            --sample_type="dcsolver" \
+            --scale="${scale}" \
+            --lower_order_final
+
+    done
+done
--- codebases/guided-diffusion/search_dc/imagenet256/search_dc_all.py
+++ codebases/guided-diffusion/search_dc/imagenet256/search_dc_all.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-N = 10
+N = 2
 for scale in [2.0, 4.0, 6.0, 8.0]:
     dc_dir = f"/data/guided-diffusion/dc/imagenet256/scale{scale}"
     os.makedirs(dc_dir, exist_ok=True)
@@ -21,7 +21,7 @@ for scale in [2.0, 4.0, 6.0, 8.0]:
 
     from sample import parse_args_and_config, Diffusion
 
-    for NFE in [5, 6, 8, 10, 12, 15, 20, 25, 30, 35, 40]:
+    for NFE in [5, 6, 8, 10, 12, 15, 20, 25]:
         for order in [3]:
             ###############################################################################
             # 1) Notebook에서 sys.argv를 직접 설정 (argparse 흉내)
